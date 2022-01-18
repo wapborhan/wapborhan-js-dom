9f62e9568af4910b2718b44ca1dc844eeea103e7
Borhan Uddin <borhaninfos@gmail.com>
2022-01-18 21:05:38 +0600

    comment out

--- assets/js/wbjs.js
+++ assets/js/wbjs.js
@@ -4,32 +4,32 @@ function addElement() {
   //Pick Root
   const root = document.getElementById("root");
 
-  // main-page-wrapper Create
+  // <main-page-wrapper>
   const mainwrapper = document.createElement("div");
   mainwrapper.classList = "main-page-wrapper";
   root.appendChild(mainwrapper);
 
-  // rn-header-image-area Create
+  // <main-page-wrapper> <rn-header-image-area>
   const rnHeader = document.createElement("div");
   rnHeader.classList = "rn-header-image-area";
   mainwrapper.appendChild(rnHeader);
 
-  // bg_image--9 bg_image h-100 Create
+  // <main-page-wrapper> <rn-header-image-area> <bg_image--9 bg_image h-100 Create>
   const bgImage9 = document.createElement("div");
   bgImage9.classList.add("bg_image--9", "bg_image", "h-100");
   rnHeader.appendChild(bgImage9);
 
-  // rn-content-wrapper create
+  // <main-page-wrapper> <rn-content-wrapper>
   const rnContent = document.createElement("div");
   rnContent.classList.add("rn-content-wrapper");
   mainwrapper.appendChild(rnContent);
 
-  // container
+  // <main-page-wrapper> <rn-content-wrapper> <container>
   const container = document.createElement("div");
   container.classList.add("container");
   rnContent.appendChild(container);
 
-  //row
+  // <main-page-wrapper> <rn-content-wrapper> <container> <row>
   const row = document.createElement("div");
   row.classList.add(
     "row",
@@ -39,28 +39,51 @@ function addElement() {
     "d-flex"
   );
   container.appendChild(row);
-  //col-lg-6
+
+  // <main-page-wrapper> <rn-content-wrapper> <container> <row> <col-lg-6> - 1
   const col6_1 = document.createElement("div");
   col6_1.classList.add("col-lg-6");
   row.appendChild(col6_1);
-
+  // <main-page-wrapper> <rn-content-wrapper> <container> <row> <col-lg-6> - 2
   const col6_2 = document.createElement("div");
   col6_2.classList.add("col-lg-6");
   row.appendChild(col6_2);
 
-  //header-left create
+  //<main-page-wrapper> <rn-content-wrapper> <container> <row> <col-lg-6> - 1 <header-left>
   const headerl = document.createElement("div");
   headerl.classList.add("header-left");
   col6_1.appendChild(headerl);
 
-  // header-thumbnail
+  // <main-page-wrapper> <rn-content-wrapper> <container> <row> <col-lg-6> - 1 <header-left> <header-thumbnail>
   const headert = document.createElement("div");
   headert.classList.add("header-thumbnail");
   headerl.appendChild(headert);
 
-  // personal image add
+  // <main-page-wrapper> <rn-content-wrapper> <container> <row> <col-lg-6> - 1 <header-left> <header-thumbnail> <img>
   const psimage = document.createElement("img");
   psimage.setAttribute("src", "assets/images/logo/banner-02.png");
   psimage.setAttribute("alt", "Borhan");
   headert.appendChild(psimage);
+
+  // <main-page-wrapper> <rn-content-wrapper> <container> <row> <col-lg-6> - 1 <header-left> <header-info-content>
+  const headerInCon = document.createElement("div");
+  headerInCon.classList.add("header-info-content");
+  headerl.appendChild(headerInCon);
+
+  // <main-page-wrapper> <rn-content-wrapper> <container> <row> <col-lg-6> - 1 <header-left> <header-info-content> <h4>
+  const headInh4 = document.createElement("h4");
+  headInh4.classList.add("title");
+  headerInCon.appendChild(headInh4);
+
+  // <main-page-wrapper> <rn-content-wrapper> <container> <row> <col-lg-6> - 1 <header-left> <header-info-content> <h4> <a>
+  const headInh4a = document.createElement("a");
+  headInh4a.setAttribute("href", "http://www.wapborhan.com");
+  headInh4a.innerText = "Borhan Uddin";
+  headInh4.appendChild(headInh4a);
+
+  // <main-page-wrapper> <rn-content-wrapper> <container> <row> <col-lg-6> - 1 <header-left> <header-info-content> <status-info>
+  const headInst = document.createElement("div");
+  headInst.classList.add("status-info");
+  headInst.innerText = "I am a Front-End Developer";
+  headInh4.appendChild(headInst);
 }
